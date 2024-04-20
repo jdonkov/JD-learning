cf2110dda6558e82d9fc74f2271972f404df4781
jdonkov <146319072+jdonkov@users.noreply.github.com>
2024-04-20 14:52:11 +0300

    Update Resume_fit.py

--- Resume_fit.py
+++ Resume_fit.py
@@ -4,19 +4,20 @@ import os
 
 # OpenAI API key
 api_key = os.getenv("OPENAI_API_KEY")
+
+# Check if the API key is set
+if not api_key:
+    st.error("OpenAI API key is not set. Please set the OPENAI_API_KEY environment variable.")
+    st.stop()
+
 client = OpenAI(api_key=api_key)
 
 # Analyzer UI
 st.title('Elevate Your Job Search: Resume Analyzer 🔎📄')
 st.markdown('Optimize your resume for specific job descriptions. Get tailored insights into your skills, experience, and education alignment')
 
-
 # Start OPENAI API
 def compare_resume_to_job_description(resume_text, job_description_text):
-    if not api_key:
-        st.error("OpenAI API key is not set. Please set it in your environment variables.")
-        return
-
     client = OpenAI(api_key=api_key)
     model = "gpt-3.5-turbo"
 
@@ -35,15 +36,11 @@ def compare_resume_to_job_description(resume_text, job_description_text):
     )
     return response.choices[0].message.content
 
-
-
 resume_text = st.text_area("Paste your resume here:", height=300)
 job_description_text = st.text_area("Paste the job description here:", height=300)
-
 
 if st.button('Analyze Resume'):
     with st.spinner('Your resume is under the microscope...'):
         result = compare_resume_to_job_description(resume_text, job_description_text)
         if result:
             st.write(result)
-
